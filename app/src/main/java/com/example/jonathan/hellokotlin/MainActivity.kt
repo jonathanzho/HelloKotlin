e9452ace5a3c8fac42229a88bb97f18b80bfc759
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-12 19:40:33 -0700

    usining Log, TAG, string resource.

--- app/src/main/java/com/example/jonathan/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/hellokotlin/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.jonathan.hellokotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "HelloKotlin: MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
