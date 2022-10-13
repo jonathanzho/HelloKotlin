6cb61c5d6d549b23b1c42e44bdd89111b1bd352d
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-12 19:59:16 -0700

    Activity life cycle functions.

--- app/src/main/java/com/example/jonathan/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/hellokotlin/MainActivity.kt
@@ -13,4 +13,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+
+        super.onDestroy()
+    }
 }
